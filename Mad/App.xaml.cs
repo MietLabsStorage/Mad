9f30d4846070c31a5b6839b027058b8239771bd1
mJaJksJ <maksim.m00@mail.ru>
2024-01-03 16:04:30 +0300

    feat: init maui project

---
+++ Mad/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Mad;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Mad/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Mad;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
